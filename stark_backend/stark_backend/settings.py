fc36e9606a9e989238611f5055476a9505e86a45
Rania-Alhajjar <rania.alhjjar@gmail.com>
2025-10-08 23:16:34 +0300

    working om agent files

--- stark_backend/stark_backend/settings.py
+++ stark_backend/stark_backend/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = [
     "dashboard",
     "payment_methods",
     'third_party_apis',
+    'system',
 ]
 
 
--- stark_backend/stark_backend/urls.py
+++ stark_backend/stark_backend/urls.py
@@ -14,8 +14,7 @@ urlpatterns = [
     path("api/payment_methods/", include("payment_methods.urls")),
     path("api/third_party_apis/", include("third_party_apis.urls")),
     path("api/payment/", include("payment.urls")),
-
-
+    path('api/system/', include('system.urls')),
 ]
 
 if settings.DEBUG:
